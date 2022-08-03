31c18bb4c67c2ce71036c6ec1b3fb1c88c20f9e8
dbdimitrov <d.boykoff@gmail.com>
2022-08-03 16:45:23 +0200

    tensor test

---
+++ test/biopsies_tensor.R
@@ -0,0 +1,7 @@
+require(liana)
+
+context_df_dict <- readRDS("data/context_df_dict.RDS")
+
+tensor <- liana_tensor_c2c(context_df_dict)
+
+saveRDS(liana::format_c2c_factors(tensor), "data/tensor_factors.RDS")
