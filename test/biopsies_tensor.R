8e9889651ab347e00c83f3ddf45c654431c1a6e5
dbdimitrov <d.boykoff@gmail.com>
2022-08-19 11:34:54 +0200

    change fact #

--- test/biopsies_tensor.R
+++ test/biopsies_tensor.R
@@ -4,8 +4,8 @@ context_df_dict <- readRDS("data/context_df_dict.RDS")
 
 tensor <- liana_tensor_c2c(context_df_dict,
                            score_col = "LRscore",
-                           conda_env = "/net/data.isilon/ag-saez/bq_ddimitrov/SOFTWARE/miniconda3/envs/cell2cell/",
-                           rank=10)
+                           conda_env = "cell2cell",
+                           rank=15)
 
 factors <- format_c2c_factors(tensor)
 
